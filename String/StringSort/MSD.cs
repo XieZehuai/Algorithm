723423aa6043a3bfc76c488a14b6f21c4fbb349f
XieZehuai <1544498268@qq.com>
2021-07-10 17:19:03 +0800

    高位优先的字符串排序

--- String/StringSort/MSD.cs
+++ String/StringSort/MSD.cs
@@ -1,33 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using _2_Sort;
 
 namespace _5_String.StringSort
 {
     /// <summary>
     /// 高位优先的字符串排序，字符串的长度可以不同
     /// </summary>
-    public class MSD
+    public class MSD : ISorter<string>
     {
-        private const int M = 15; // 子数组长度小于该值时切换为插入排序
+        private const int R = 26; // 基数
+
+        private readonly int threshold; // 子数组长度小于该值时切换排序算法
+        private readonly ISorter<string> sorter; // 子数组长度小于 threshold 时使用的排序算法
         private string[] aux; // 数据分类的辅助数组
+
+        public MSD(ISorter<string> sorter, int threshold)
+        {
+            this.sorter = sorter;
+            this.threshold = threshold;
+        }
+
+        public string Name => "高位优先的字符串排序";
 
         public void Sort(string[] data)
         {
+            int length = data.Length;
+            aux = new string[length];
+            Sort(data, 0, length - 1, 0);
+        }
 
+        public void Sort(string[] data, int low, int high)
+        {
+            int length = data.Length;
+            aux = new string[length];
+            Sort(data, low, high, 0);
+        }
+
+        private void Sort(string[] data, int low, int high, int d)
+        {
+            // 排序元素个数小于 M 时用插入排序
+            if (high <= low + threshold)
+            {
+                sorter.Sort(data, low, high);
+            }
+            else
+            {
+                int[] count = new int[R + 2];
+
+                for (int i = low; i <= high; i++)
+                {
+                    count[CharAt(data[i], d) + 1]++;
+                }
+
+                for (int r = 0; r <= R; r++)
+                {
+                    count[r + 1] += count[r];
+                }
+
+                for (int i = low; i <= high; i++)
+                {
+                    aux[count[CharAt(data[i], d)]++] = data[i];
+                }
+
+                for (int i = low; i <= high; i++)
+                {
+                    data[i] = aux[i - low];
+                }
+
+                for (int r = 0; r < R; r++)
+                {
+                    Sort(data, low + count[r], low + count[r + 1] - 1, d + 1);
+                }
+            }
         }
 
         private int CharAt(string str, int index)
         {
             if (index < str.Length)
             {
-                return str[index];
+                return str[index] - 'a' + 1;
             }
             else
             {
-                return -1;
+                return 0;
             }
         }
     }
--- String/StringSort/Test.cs
+++ String/StringSort/Test.cs
@@ -14,45 +14,81 @@ namespace _5_String.StringSort
 
         public static void Invoke()
         {
-            string[] rawData = GenerateTestData(LENGTH, W);
+            //string[] rawData = GenerateTestData(10, 5, 10);
+            //CorrectnessTest(new MSD(15), rawData);
 
-            SortTest(new LSD(W), rawData);
-            SortTest(new CSharpSort<string>(), rawData);
+            string[] rawData = GenerateTestData(LENGTH, 20);
+            EfficiencyTest(new LSD(W), rawData);
+            EfficiencyTest(new MSD(new QuickSort<string>(), 10), rawData);
+            EfficiencyTest(new CSharpSort<string>(), rawData);
         }
 
-        private static void SortTest(ISorter<string> sorter, string[] originData)
+        private static void CorrectnessTest(ISorter<string> sorter, string[] rawData)
         {
-            string[] data = new string[originData.Length];
-            Array.Copy(originData, data, originData.Length);
-            Sort(sorter, data);
+            string[] data1 = new string[rawData.Length];
+            Array.Copy(rawData, data1, rawData.Length);
+            sorter.Sort(data1);
+
+            string[] data2 = new string[rawData.Length];
+            Array.Copy(rawData, data2, rawData.Length);
+            new CSharpSort<string>().Sort(data2);
+
+            bool result = true;
+            for (int i = 0; i < rawData.Length; i++)
+            {
+                if (data1[i] != data2[i])
+                {
+                    result = false;
+                }
+            }
+
+            Console.WriteLine(result);
         }
 
-        private static void Sort<T>(ISorter<T> sorter, T[] data)
+        // 效率测试
+        private static void EfficiencyTest(ISorter<string> sorter, string[] rawData)
         {
+            // 复制原始数据
+            string[] data = new string[rawData.Length];
+            Array.Copy(rawData, data, rawData.Length);
+
+            // 排序并计时
             sw.Restart();
             sorter.Sort(data);
             sw.Stop();
 
-            Console.WriteLine($"{sorter.Name}\t{sw.Elapsed.TotalMilliseconds}毫秒");
+            // 打印结果
+            Console.WriteLine($"{sorter.Name}\t{sw.Elapsed.TotalMilliseconds}\t毫秒");
         }
 
+        // 生成 length 个长度为 w 的随机字符串
         private static string[] GenerateTestData(int length, int w)
         {
-            string[] str = new string[length];
+            return GenerateTestData(length, w, w);
+        }
+
+        // 生成 length 个长度为 minW 到 maxW 之间的随机字符串
+        private static string[] GenerateTestData(int length, int minW, int maxW)
+        {
+            string[] data = new string[length];
             Random random = new Random();
+            StringBuilder sb = new StringBuilder();
+            int delta = maxW - minW + 1;
 
             for (int i = 0; i < length; i++)
             {
-                StringBuilder sb = new StringBuilder();
+                int w = random.Next() % delta + minW;
                 for (int j = 0; j < w; j++)
                 {
                     char c = (char)(random.Next() % 26 + 'a');
                     sb.Append(c);
                 }
-                str[i] = sb.ToString();
+
+                data[i] = sb.ToString();
+                sb.Clear();
             }
 
-            return str;
+            return data;
         }
     }
 }
