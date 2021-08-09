adf84da106a5c8667097c67c5c848651271030e6
XieZehuai <1544498268@qq.com>
2021-08-09 16:25:44 +0800

    单独的优先队列实现

--- 2 Sort/Program.cs
+++ 2 Sort/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace _2_Sort
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2_Sort
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            PriorityQueue.PriorityQueueTest.Test();
+            Queue<int> q = new Queue<int>();
+            int[] data = new int[20];
+            Random rd = new Random();
+            PriorityQueue<int> queue = new PriorityQueue<int>(Compare: (a, b) =>
+            {
+                return b - a;
+            });
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i;
+                q.Enqueue(i * 2);
+                Console.Write(data[i] + " ");
+            }
+            Console.WriteLine();
+
+            int k = 10;
+            for (int i = 0; i < k; i++)
+            {
+                queue.Enqueue(data[i]);
+            }
+
+            for (int i = k; i < data.Length; i++)
+            {
+                if (data[i] > queue.Peek())
+                {
+                    queue.Dequeue();
+                    queue.Enqueue(data[i]);
+                }
+            }
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    internal class Test : IComparable<Test>
+    {
+        public int id;
+
+        public Test(int id)
+        {
+            this.id = id;
+        }
+
+        public int CompareTo(Test other)
+        {
+            return id - other.id;
+        }
+
+        public override string ToString()
+        {
+            return id.ToString();
         }
     }
 }
